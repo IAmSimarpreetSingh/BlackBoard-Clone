0e5e82d48aabff5f0c63153c10faf4e7f2d8870c
Simarpreet Singh <iamsimarpreetsingh@gmail.com>
2021-11-08 21:31:19 +0530

    token updated

--- blackboard/src/settings.js
+++ blackboard/src/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 
 const appID = "a5ec9b31327948cc8b5d65c47bf643c8";
-const token = "006a5ec9b31327948cc8b5d65c47bf643c8IAD9lov5QPknP3OFPu/9S2ntk3NQhQpgzcrwGHu18d6qClpiGh8AAAAAEACkCrtyyP2HYQEAAQDYC4hh";
+const token = "006a5ec9b31327948cc8b5d65c47bf643c8IACfou4AJhhpBSli4J55figzzH1p6xU4p951sJIh9Oy2lFpiGh8AAAAAEACkCrtyAZuKYQEAAQAAm4ph";
 
 
 export const config = {mode: "rtc", codec: "vp8", appId: appID, token: token };
